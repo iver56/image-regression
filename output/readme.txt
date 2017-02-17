510f973f4c33ff7309cf98d5866ed5e0aff2034b
iver56 <iver56@hotmail.com>
2017-02-18 00:32:33 +0100

    Store checkpoint outputs

---
+++ output/readme.txt
@@ -0,0 +1,1 @@
+Checkpoint output images go here
--- train.py
+++ train.py
@@ -2,12 +2,15 @@ from __future__ import division
 from skimage.io import imread, imsave
 from keras.models import Sequential
 from keras.layers import Activation, Dense
+from keras.optimizers import SGD
+from keras.callbacks import Callback
 import numpy as np
+import warnings
+import os
 
-file_path = 'keyboard.png'
-image = imread(file_path, as_grey=True)
+image_filename = 'keyboard.png'
+image = imread(image_filename, as_grey=True)
 img_width, img_height = image.shape
-
 
 x = []
 y = []
@@ -32,25 +35,41 @@ model.add(Activation('relu'))
 
 model.compile(
     loss='mean_squared_error',
-    optimizer='rmsprop'
+    optimizer=SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 )
 
+
+class CheckpointOutputs(Callback):
+    def __init__(self):
+        super(CheckpointOutputs, self).__init__()
+        self.last_loss_checkpoint = 9001  # it's over 9000!
+        self.loss_change_threshold = 0.01
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            return
+        loss_change = 1 - logs['loss'] / self.last_loss_checkpoint
+
+        if loss_change > self.loss_change_threshold:
+            self.last_loss_checkpoint = logs['loss']
+            predicted_image = self.model.predict(x, verbose=False)
+            predicted_image = np.clip(predicted_image, 0, 1)
+            predicted_image = predicted_image.reshape(image.shape)
+
+            with warnings.catch_warnings():
+                output_file_path = os.path.join(
+                    'output',
+                    'keyboard_predicted_{}.png'.format(epoch)
+                )
+                imsave(output_file_path, predicted_image)
+
+
+checkpoint_outputs = CheckpointOutputs()
 history = model.fit(
     x,
     y,
-    batch_size=64,
-    nb_epoch=300,
-    shuffle=True
+    batch_size=128,
+    nb_epoch=900,
+    shuffle=True,
+    callbacks=[checkpoint_outputs]
 )
-print(history)
-
-predicted_image = np.copy(image)
-for i in range(img_height):
-    for j in range(img_width):
-        predicted_image[i][j] = model.predict_proba(
-            np.array([[i / img_height, j / img_width]]),
-            verbose=False
-        )[0]
-predicted_image = np.clip(predicted_image, 0, 1)
-
-imsave('keyboard_predicted.png', predicted_image)
